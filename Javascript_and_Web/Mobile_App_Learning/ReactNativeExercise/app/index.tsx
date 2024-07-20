91a330b130bad0eda591697345a6afc15fc03236
Athanasios Topaltsis <a.topaltsis@gmail.com>
2024-07-20 14:55:56 -0400

    working on react native exercise

--- Javascript_and_Web/Mobile_App_Learning/ReactNativeExercise/app/index.tsx
+++ Javascript_and_Web/Mobile_App_Learning/ReactNativeExercise/app/index.tsx
@@ -1,15 +1,6 @@
+import WelcomeScreen from "@/components/WelcomeScreen";
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <WelcomeScreen />;
 }
